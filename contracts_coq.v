ee7d2e05a8db7cfba1f4e69c536254d09d988cab
Kiarahmani <rahmank@purdue.edu>
2016-02-17 18:44:05 -0500

    completed cont

--- contracts_coq.v
+++ contracts_coq.v
@@ -16,17 +16,17 @@ Require Import contract_Eval.
 
 
 Parameter var_equal : forall x y : EffVar, {x = y} + {x <> y}.
+Parameter Eff_name : Effect -> string.
 
-Fixpoint Prop_Evaluation (pr:contract_Prop)(Ex:Exec) : Eval_result :=
+Fixpoint Prop_Evaluation (pr:contract_Prop)(Ex:Exec) : Prop:=
   match pr with
-    |contract_prop_true => result True
-    |contract_prop_effeff R1 α β      => result((relation_Evaluation R1 Ex) α β)
-    |contract_prop_conjunction p1 p2  => And_result (Prop_Evaluation p1 Ex) (Prop_Evaluation p2 Ex)
-    |contract_prop_disjunction p1 p2  => Or_result  (Prop_Evaluation p1 Ex) (Prop_Evaluation p2 Ex)
-    |contract_prop_implication p1 p2  => Imp_result (Prop_Evaluation p1 Ex) (Prop_Evaluation p2 Ex)
-    |_=> error                                                             
+    |contract_prop_true => True
+    |contract_prop_effeff R1 α β      => (relation_Evaluation R1 Ex) α β
+    |contract_prop_conjunction p1 p2  =>  (Prop_Evaluation p1 Ex) /\ (Prop_Evaluation p2 Ex)
+    |contract_prop_disjunction p1 p2  =>  (Prop_Evaluation p1 Ex) \/  (Prop_Evaluation p2 Ex)
+    |contract_prop_implication p1 p2  =>  (Prop_Evaluation p1 Ex) ->  (Prop_Evaluation p2 Ex)
+    |_=> False                                                           
   end.
-
 
 
 Fixpoint Prop_Subst (pr:contract_Prop)(eff:Effect)(v:EffVar) : contract_Prop :=
@@ -41,18 +41,35 @@ Fixpoint Prop_Subst (pr:contract_Prop)(eff:Effect)(v:EffVar) : contract_Prop :=
                                      else (if (var_equal v v2) then contract_prop_vareff R1 v1 eff
                                            else contract_prop_varvar R1 v1 v2)
     |contract_prop_disjunction p1 p2 => contract_prop_disjunction (Prop_Subst p1 eff v) (Prop_Subst p2 eff v)
-    |contract_prop_conjunction p1 p2 => contract_prop_conjunctieson (Prop_Subst p1 eff v) (Prop_Subst p2 eff v)
+    |contract_prop_conjunction p1 p2 => contract_prop_conjunction (Prop_Subst p1 eff v) (Prop_Subst p2 eff v)
     |contract_prop_implication p1 p2 => contract_prop_implication (Prop_Subst p1 eff v) (Prop_Subst p2 eff v)                             
   end.
 
+Fixpoint contract_Subst (ct:contract_Contract) (eff:Effect)(v:EffVar) : contract_Contract :=
+  match ct with
+    |contract_free_cons π => contract_free_cons (Prop_Subst π eff v)
+    |contract_typed_cons e τ ψ => contract_Subst ψ eff v
+  end.
+Variable x :EffVar.
 
-Fixpoint contract_Evaluation (cont: contract_Contract) (Ex:Exec) :Prop :=
-  match cont with
-    |contract_free_cons π => Prop_Evaluation π Ex
-    |contract_untyped_cons e ψ => 
-    |contract_typed_cons e τ ψ => True
+
+
+Fixpoint contract_Eval (cont: contract_Contract) (Ex:Exec) (n:nat):Prop :=
+  match n with
+    |O => match cont with
+           |contract_free_cons π => Prop_Evaluation π Ex
+           |_=> False
+         end
+           
+    |S n' => match cont with
+              |contract_typed_cons e τ ψ =>  forall e:Effect,(Ex-A e)-> contract_Eval (contract_Subst ψ e (effvar (Eff_name e))) Ex n'
+              |_=>False
+            end
   end.
 
+                       
+                                                                            
 
+   
 
 
